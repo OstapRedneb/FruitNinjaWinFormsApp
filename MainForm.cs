edfdb0b5646cee6908b2cc2dd97c6a149520ad41
OstapRedneb <rodionzabirov888@gmail.com>
2025-08-15 12:03:22 +0500

    Добавьте файлы проекта.

---
+++ MainForm.cs
@@ -0,0 +1,30 @@
+namespace FruitNinjaWinFormsApp
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void settingsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void resultsButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
