08e837096d2d2ff98d34f2b400619175f57561a8
OstapRedneb <rodionzabirov888@gmail.com>
2025-08-15 17:09:16 +0500

    Добавил пустые формы и вызвал их из меню. Подключил библиотеку 2048

--- MainForm.cs
+++ MainForm.cs
@@ -9,22 +9,25 @@ namespace FruitNinjaWinFormsApp
 
         private void startButton_Click(object sender, EventArgs e)
         {
-
+            new UserForm().Show();
+            Close();
         }
 
         private void settingsButton_Click(object sender, EventArgs e)
         {
-
+            new SettingsForm().Show();
+            Close();
         }
 
         private void resultsButton_Click(object sender, EventArgs e)
         {
-
+            new ResultsForm().Show();
+            Close();
         }
 
         private void exitButton_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
     }
 }
