75ea44a00b60348cc844181206ab779d7a1774cb
OstapRedneb <rodionzabirov888@gmail.com>
2025-08-15 19:28:22 +0500

    Формочка настроек, и её код

--- SettingsForm.Designer.cs
+++ SettingsForm.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SettingsForm";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
+            backButton = new Button();
+            showLabel = new Label();
+            hScrollBar1 = new HScrollBar();
+            dificultLabel = new Label();
+            clearButton = new Button();
+            SuspendLayout();
+            // 
+            // backButton
+            // 
+            backButton.BackColor = Color.FromArgb(32, 32, 32);
+            backButton.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold);
+            backButton.ForeColor = Color.FromArgb(255, 128, 0);
+            backButton.Location = new Point(659, 382);
+            backButton.Name = "backButton";
+            backButton.Size = new Size(129, 56);
+            backButton.TabIndex = 4;
+            backButton.Text = "Назад";
+            backButton.UseVisualStyleBackColor = false;
+            backButton.Click += backButton_Click;
+            // 
+            // showLabel
+            // 
+            showLabel.AutoSize = true;
+            showLabel.BackColor = Color.FromArgb(64, 64, 64);
+            showLabel.Font = new Font("Comic Sans MS", 24F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            showLabel.ForeColor = Color.FromArgb(255, 128, 0);
+            showLabel.Location = new Point(95, 45);
+            showLabel.Name = "showLabel";
+            showLabel.Size = new Size(200, 45);
+            showLabel.TabIndex = 5;
+            showLabel.Text = "Сложность:";
+            // 
+            // hScrollBar1
+            // 
+            hScrollBar1.LargeChange = 1;
+            hScrollBar1.Location = new Point(311, 45);
+            hScrollBar1.Name = "hScrollBar1";
+            hScrollBar1.Size = new Size(211, 45);
+            hScrollBar1.TabIndex = 6;
+            hScrollBar1.Scroll += hScrollBar1_Scroll;
+            // 
+            // dificultLabel
+            // 
+            dificultLabel.AutoSize = true;
+            dificultLabel.BackColor = Color.FromArgb(64, 64, 64);
+            dificultLabel.Font = new Font("Comic Sans MS", 24F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            dificultLabel.ForeColor = Color.Lime;
+            dificultLabel.Location = new Point(543, 45);
+            dificultLabel.Name = "dificultLabel";
+            dificultLabel.Size = new Size(112, 45);
+            dificultLabel.TabIndex = 7;
+            dificultLabel.Text = "Легко";
+            // 
+            // clearButton
+            // 
+            clearButton.BackColor = Color.Red;
+            clearButton.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold);
+            clearButton.ForeColor = Color.White;
+            clearButton.Location = new Point(311, 382);
+            clearButton.Name = "clearButton";
+            clearButton.Size = new Size(240, 56);
+            clearButton.TabIndex = 8;
+            clearButton.Text = "Удалить весь прогресс";
+            clearButton.UseVisualStyleBackColor = false;
+            clearButton.Click += clearButton_Click;
+            // 
+            // SettingsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(64, 64, 64);
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(clearButton);
+            Controls.Add(dificultLabel);
+            Controls.Add(hScrollBar1);
+            Controls.Add(showLabel);
+            Controls.Add(backButton);
+            Name = "SettingsForm";
+            Text = "SettingsForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button backButton;
+        private Label showLabel;
+        private HScrollBar hScrollBar1;
+        private Label dificultLabel;
+        private Button clearButton;
     }
 }
--- SettingsForm.cs
+++ SettingsForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _2048ClassLibrary;
 
 namespace FruitNinjaWinFormsApp
 {
@@ -15,6 +16,30 @@ namespace FruitNinjaWinFormsApp
         public SettingsForm()
         {
             InitializeComponent();
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            switch (e.NewValue) // Добавь потом логику
+            {
+                case < 33: break;
+                case > 33 and < 66: break;
+                case < 66: break;
+
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            DialogResult answer = MessageBox.Show("Вы уверены, что хотите удалить все результаты?\nПрогресс всех игроков будет безвозвратно утерян", "Предупреждение!", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (answer == DialogResult.OK)
+                FileWorker.Clear();
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            new MenuForm().Show();
+            Close();
         }
     }
 }
